ee6775bd565300d83aad452ffa1a0ecb7dce0ac5
zyg <6234149Asd>
2022-01-22 14:36:31 +0800

    路由的配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,18 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", name: "index", component: () => import("@/views/Index.vue") },
-  { path: "/hostGraphics", name: "hostGraphics", component: () => import("@/views/Graphics.vue") },
-
+  {
+    path: "/",
+    name: "index",
+    component: () => import("@/views/Index.vue"),
+    meta: { title: "接口视图监控" },
+  },
+  {
+    path: "/hostGraphics",
+    name: "hostGraphics",
+    component: () => import("@/views/Graphics.vue"),
+    meta: { title: "接口视图监控" },
+  },
 ];
 
 const router = new VueRouter({
